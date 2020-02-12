1c5672a9b64f528b1b92bb2a310d74b9a757c434
themebing <themebing@gmail.com>
2020-02-12 11:50:23 +0600

    up

--- includes/elementor/elementor.php
+++ includes/elementor/elementor.php
@@ -50,6 +50,7 @@ class gemas_ElementorCustomElement {
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-title.php');
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-accordion.php');
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-counter.php');
+         include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-feature.php');
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-new.php');
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-partner.php');
          include_once(plugin_dir_path( __FILE__ ).'/widgets/widget-products.php');
--- includes/elementor/widgets/widget-feature.php
+++ includes/elementor/widgets/widget-feature.php
@@ -32,63 +32,100 @@ class gemas_Widget_Feature extends Widget_Base {
          ]
       );
 
+
+      $this->add_control(
+      'bg_image',
+        [
+          'label' => __( 'Background image', 'gemas' ),
+          'type' => \Elementor\Controls_Manager::MEDIA,
+          'default' => [
+            'url' => get_template_directory_uri().'/images/features_bg.jpg',
+          ],
+        ]
+      );
+
+
+
+      $this->add_control(
+         'sub-title',
+         [
+            'label' => __( 'Sub Title', 'gemas' ),
+            'type' => \Elementor\Controls_Manager::TEXTAREA,
+            'default' => __('What We do','gemas')
+         ]
+      );
+
+
+      $this->add_control(
+         'title',
+         [
+            'label' => __( 'Title', 'gemas' ),
+            'type' => \Elementor\Controls_Manager::TEXT,
+            'default' => __('Lowest Pricing Available Online or Brand Themes Products Time','gemas')
+         ]
+      );
+
+
+
+      $this->add_control(
+         'text',
+         [
+            'label' => __( 'Text', 'gemas' ),
+            'type' => \Elementor\Controls_Manager::TEXTAREA,
+            'default' => __('Find technology or people for digital projects in public sector. Find an individual specialist developer.','gemas')
+         ]
+      );
+      
+
+      $this->add_control(
+         'button_text', [
+            'label' => __( 'Button Text', 'gemas' ),
+            'type' => \Elementor\Controls_Manager::TEXT,
+            'default' => __('More Features','gemas')
+         ]
+      );
+
+      $this->add_control(
+         'button_url', [
+            'label' => __( 'Button URL', 'gemas' ),
+            'type' => \Elementor\Controls_Manager::TEXT,
+            'default' => '#'
+         ]
+      );
+      
+
+
       $repeater = new \Elementor\Repeater();
 
       $repeater->add_control(
          'image',
          [
-            'label' => __( 'Choose Photo', 'gemas' ),
+            'label' => __( 'Choose icon', 'gemas' ),
             'type' => \Elementor\Controls_Manager::MEDIA,
             'default' => [
                'url' => \Elementor\Utils::get_placeholder_image_src()
             ]
          ]
       );
-      
+
       $repeater->add_control(
-         'name',
+         'title',
          [
-            'label' => __( 'Name', 'gemas' ),
+            'label' => __( 'Title', 'gemas' ),
             'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Emaley Mcculloch', 'gemas' ),
-            
+            'default' => __( 'Experienced 10 Years!', 'gemas' ),
          ]
       );
 
       $repeater->add_control(
-         'designation',
+         'desc',
          [
-            'label' => __( 'Designation', 'gemas' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Founder ceo', 'gemas' ),
-         ]
-      );
-
-      $repeater->add_control(
-         'feature',
-         [
-            'label' => __( 'feature', 'gemas' ),
+            'label' => __( 'Description', 'gemas' ),
             'type' => \Elementor\Controls_Manager::TEXTAREA,
-            'default' => __( 'In promotion and of advertising feature show consiss of a person\'s written orsoken statement extollig the virtue', 'gemas' ),
+            'default' => __( 'Find technology or people for digital projects in public sector.', 'gemas' ),
          ]
       );
 
-      $repeater->add_control(
-        'rating',
-        [
-          'label' => __( 'Rating', 'gemas' ),
-          'type' => \Elementor\Controls_Manager::SELECT,
-          'default' => 1,
-          'options' => [
-            1 => __( 'Star 1', 'gemas' ),
-            2 => __( 'Star 2', 'gemas' ),
-            3 => __( 'Star 3', 'gemas' ),
-            4 => __( 'Star 4', 'gemas' ),
-            5 => __( 'Star 5', 'gemas' ),
-            'none' => __( 'None', 'gemas' ),
-          ]
-        ]
-      );
 
       $this->add_control(
          'feature_list',
@@ -96,7 +133,7 @@ class gemas_Widget_Feature extends Widget_Base {
             'label' => __( 'feature List', 'gemas' ),
             'type' => \Elementor\Controls_Manager::REPEATER,
             'fields' => $repeater->get_controls(),
-            'title_field' => '{{name}}',
+            'title_field' => '{{title}}',
 
          ]
       );
@@ -112,31 +149,31 @@ class gemas_Widget_Feature extends Widget_Base {
       $settings = $this->get_settings_for_display(); ?>
 
       <!-- features-area -->
-      <section class="features-area features-bg pt-120 pb-65">
+      <section class="features-area features-bg pt-120 pb-65" style="background-image: url(<?php echo esc_url($settings['bg_image']['url']) ?>);">
           <div class="container">
               <div class="row">
                   <div class="col-lg-6">
                       <div class="section-title white-title mb-40">
-                          <span>What We do</span>
-                          <h2>Lowest Pricing Available Online or Brand Themes Products Time</h2>
+                          <span><?php echo esc_html($settings['sub-title']); ?></span>
+                          <h2><?php echo esc_html($settings['title']); ?></h2>
                       </div>
                       <div class="features-btn">
-                          <a href="#" class="btn">More Features</a>
+                          <a href="<?php echo esc_url($settings['button_url']); ?>" class="btn"><?php echo esc_html($settings['button_text']); ?></a>
                       </div>
                   </div>
                   <div class="col-lg-6">
                       <div class="features-top-content">
-                          <p>Find technology or people for digital projects in public sector. Find an individual specialist developer.</p>
+                          <p><?php echo esc_html($settings['text']); ?></p>
                           <div class="row">
                             <?php foreach (  $settings['feature_list'] as $feature_single ): ?>
                               <div class="col-md-6">
                                   <div class="features-box mb-50">
                                       <div class="features-icon mb-20">
-                                          <img src="img/icon/features_icon01.png" alt="">
+                                          <img src="<?php echo esc_html($feature_single['image']['url']); ?>" alt="<?php echo esc_attr($feature_single['title']); ?>">
                                       </div>
                                       <div class="features-box-content">
-                                          <h5><?php echo esc_html($feature_single['name']); ?></h5>
-                                          <p><?php echo esc_html($feature_single['name']); ?></p>
+                                          <h5><?php echo esc_html($feature_single['title']); ?></h5>
+                                          <p><?php echo esc_html($feature_single['desc']); ?></p>
                                       </div>
                                   </div>
                               </div>
