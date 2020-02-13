3b3bb75abee382a81e17ded03407db4e65b5c196
themebing <themebing@gmail.com>
2020-02-13 10:14:33 +0600

    Update widget-blog.php

--- includes/elementor/widgets/widget-blog.php
+++ includes/elementor/widgets/widget-blog.php
@@ -54,6 +54,66 @@ class gemas_Widget_Blog extends Widget_Base {
       //Inline Editing
       $this->add_inline_editing_attributes( 'ppp', 'basic' );
       ?>
+
+
+      <!-- blog-area -->
+      <section class="blog-area gray-bg pt-120 pb-90">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-6">
+                      <div class="section-title mb-50">
+                          <span>latest news & blog</span>
+                          <h2>Recent News.</h2>
+                      </div>
+                  </div>
+              </div>
+              <div class="row">
+
+              <?php
+              $blog = new \WP_Query( array( 
+                'post_type' => 'post',
+                'posts_per_page' => 3,
+                'ignore_sticky_posts' => true,
+                'order' => $settings['order'],
+              ));
+              /* Start the Loop */
+              while ( $blog->have_posts() ) : $blog->the_post();
+              ?>
+
+                  <div class="col-lg-4 col-md-6">
+                      <div class="blog-post">
+                          <div class="blog-thumb">
+                              <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'gemas-404x297'); ?>" alt="<?php the_title_attribute() ?>"></a>
+                          </div>
+                          <div class="blog-content">
+                              <div class="blog-meta">
+                                  <ul>
+                                      <li><i class="far fa-clock"></i><?php the_date() ?> <?php echo esc_html__( 'in','digimarket' ) ?>  <?php the_category() ?></li>
+                                      <li><i class="fas fa-comment"></i>03</li>
+                                      <li><i class="fas fa-heart"></i>26</li>
+                                  </ul>
+                              </div>
+                              <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+                              <p><?php echo wp_trim_words( get_the_content(), 16, '.' ); ?></p>
+                              <div class="blog-post-avatar">
+                                  <div class="blog-avatar-img">
+                                      <img src="img/blog/blog_avatar_img.png" alt="">
+                                  </div>
+                                  <div class="blog-avatar-info">
+                                      <a href="#">- <?php the_author() ?></a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+
+              </div>
+          </div>
+      </section>
+      <!-- blog-area-end -->
+
       
          <div class="row justify-content-center">
                <?php
